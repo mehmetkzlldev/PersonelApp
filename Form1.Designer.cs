cb20391f3c50a01e8cecb0483e330f084560262f
mehmetalikzlldev <mehmetalikizoglu188@gmail.com>
2025-02-26 11:29:34 +0300

    Form olusturuldu.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,270 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            listBox1 = new ListBox();
+            label6 = new Label();
+            lblad = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(128, 128, 255);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(666, 42);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.profile_user;
+            pictureBox1.Location = new Point(3, 6);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(43, 30);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(52, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Personel Takibi";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Silver;
+            panel2.Controls.Add(listBox1);
+            panel2.Controls.Add(label6);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 42);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(205, 381);
+            panel2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.Silver;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(3, 21);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(195, 349);
+            listBox1.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Silver;
+            label6.Dock = DockStyle.Top;
+            label6.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(0, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(144, 18);
+            label6.TabIndex = 0;
+            label6.Text = "Personel Listesi";
+            // 
+            // lblad
+            // 
+            lblad.AutoSize = true;
+            lblad.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblad.Location = new Point(350, 81);
+            lblad.Name = "lblad";
+            lblad.Size = new Size(25, 17);
+            lblad.TabIndex = 2;
+            lblad.Text = "Ad";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(381, 80);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(185, 18);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(333, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 17);
+            label2.TabIndex = 2;
+            label2.Text = "Soyad";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(381, 104);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(185, 18);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(350, 129);
+            label3.Name = "label3";
+            label3.Size = new Size(28, 17);
+            label3.TabIndex = 2;
+            label3.Text = "Yas";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(381, 128);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(36, 18);
+            textBox3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(331, 197);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 17);
+            label4.TabIndex = 2;
+            label4.Text = "Telefon";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(388, 197);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(185, 18);
+            textBox4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(342, 222);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 17);
+            label5.TabIndex = 2;
+            label5.Text = "Adres";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(388, 221);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(185, 101);
+            textBox5.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Yellow;
+            button1.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(381, 355);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Yeni";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Lime;
+            button2.Font = new Font("Verdana", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(476, 355);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Ekle/Guncelle";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(287, 355);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "Sil";
+            button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 128, 255);
             ClientSize = new Size(666, 423);
+            Controls.Add(button2);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(lblad);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label lblad;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox5;
+        private ListBox listBox1;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
