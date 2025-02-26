5cb207bb00c3a6e34d3dd733d648cc28c9645312
mehmetalikzlldev <mehmetalikizoglu188@gmail.com>
2025-02-26 10:48:34 +0300

    Add project files.

---
+++ Personel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonelApp
+{
+    public class Personel
+    {
+        public string ad;
+        public string soyad;
+        public int yas;
+        public string tel;
+        public string adres;
+
+    }
+}
